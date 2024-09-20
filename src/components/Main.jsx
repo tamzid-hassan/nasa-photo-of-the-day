5599af87ef05d50cde692c321997e85a37382051
tamzid.hassan <iamtamzid96+github@gmail.com>
2024-09-20 21:09:43 +0600

    Dynamic View width and height is set to fix image proportion

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 function Main({ data }) {
     return (
         <div className="imgContainer">
-            <img src={data.hdurl} alt={data.title || "nasa picture of the day"} className="bgImage" />
+            <img loading="lazy" src={data.hdurl} alt={data.title || "nasa picture of the day"} className="bgImage" />
         </div>
     )
 }
